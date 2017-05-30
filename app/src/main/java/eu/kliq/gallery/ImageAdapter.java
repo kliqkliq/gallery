a53efcced2854635690b8c70ee81f116732af521
Sebastian Klikowicz <kliqkliq@gmail.com>
2017-05-30 17:07:22 +0200

    Progress bar reimplementation

--- app/src/main/java/eu/kliq/gallery/ImageAdapter.java
+++ app/src/main/java/eu/kliq/gallery/ImageAdapter.java
@@ -1,7 +1,7 @@
 package eu.kliq.gallery;
 
-import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v4.view.PagerAdapter;
 import android.util.Log;
 import android.view.View;
@@ -9,36 +9,21 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-import com.bumptech.glide.DrawableRequestBuilder;
+import com.bumptech.glide.BitmapTypeRequest;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.integration.okhttp.OkHttpUrlLoader;
-import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.Target;
 import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.github.chrisbanes.photoview.PhotoView;
-import com.squareup.okhttp.Interceptor;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.ResponseBody;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-
-import okio.Buffer;
-import okio.BufferedSource;
-import okio.ForwardingSource;
-import okio.Okio;
-import okio.Source;
 
 public class ImageAdapter extends PagerAdapter {
 
     public static final String LOG_TAG = "ImageAdapter";
 
     private Context mContext;
-    private ImageView mImageView;
     private ArrayList<String> mImages;
-    private OkHttpClient mOkHttpClient;
 
     ImageAdapter(Context context, ArrayList<String> images) {
         mContext = context;
@@ -63,16 +48,16 @@ public class ImageAdapter extends PagerAdapter {
                 RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
         layout.setLayoutParams(layoutParams);
 
-        mImageView = new PhotoView(mContext);
-        mImageView.setLayoutParams(layoutParams);
-        mImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        layout.addView(mImageView);
+        final ImageView imageView = new PhotoView(mContext);
+        imageView.setLayoutParams(layoutParams);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        layout.addView(imageView);
 
         String baseUrl = mImages.get(i);
         String thumbURL = JsonItem.getThumbUrl(baseUrl);
         String imageURL = JsonItem.getImageUrl(baseUrl);
 
-        DrawableRequestBuilder<String> thumbnailRequest = Glide.with(mContext).load(thumbURL);
+        BitmapTypeRequest<String> thumbnailRequest = Glide.with(mContext).load(thumbURL).asBitmap();
 
         final NumberProgressBar progressBar = new NumberProgressBar(mContext);
         progressBar.setVisibility(View.GONE);
@@ -80,47 +65,9 @@ public class ImageAdapter extends PagerAdapter {
 
         container.addView(layout, 0);
 
-        mOkHttpClient = new OkHttpClient();
-
-        final ProgressListener progressListener = new ProgressListener() {
-            @Override
-            public void update(long bytesRead, long contentLength, boolean done) {
-                final Activity activity = (Activity) mContext;
-                if (done) {
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            progressBar.setVisibility(View.GONE);
-                        }
-                    });
-                    Log.i(LOG_TAG, "Loading done");
-                } else {
-                    final int progress = (int) ((100 * bytesRead) / contentLength);
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            progressBar.setVisibility(View.VISIBLE);
-                            progressBar.setProgress(progress);
-                        }
-                    });
-                    Log.v(LOG_TAG, "Progress: " + progress + "%");
-                }
-            }
-        };
-
-        mOkHttpClient.networkInterceptors().add(new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Response originalResponse = chain.proceed(chain.request());
-                return originalResponse.newBuilder()
-                        .body(new ProgressResponseBody(originalResponse.body(), progressListener))
-                        .build();
-            }
-        });
-
-        Glide.get(mContext).register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(mOkHttpClient));
-
-        Glide.with(mContext).load(imageURL).thumbnail(thumbnailRequest).dontAnimate().into(mImageView);
+        final ProgressTarget<String, Bitmap> target = new MyProgressTarget<>(new BitmapImageViewTarget(imageView), progressBar);
+        target.setModel(imageURL);
+        Glide.with(mContext).load(imageURL).asBitmap().thumbnail(thumbnailRequest).dontAnimate().into(target);
 
         return layout;
     }
@@ -130,52 +77,27 @@ public class ImageAdapter extends PagerAdapter {
         container.removeView((RelativeLayout) obj);
     }
 
-    private static class ProgressResponseBody extends ResponseBody {
-
-        private final ResponseBody responseBody;
-        private final ProgressListener progressListener;
-        private BufferedSource bufferedSource;
-
-        public ProgressResponseBody(ResponseBody responseBody, ProgressListener progressListener) {
-            this.responseBody = responseBody;
-            this.progressListener = progressListener;
+    private static class MyProgressTarget<Z> extends ProgressTarget<String, Z> {
+        private final NumberProgressBar progress;
+        public MyProgressTarget(Target<Z> target, NumberProgressBar progress) {
+            super(target);
+            this.progress = progress;
         }
 
-        @Override
-        public MediaType contentType() {
-            return responseBody.contentType();
+        @Override public float getGranualityPercentage() {
+            return 0.1f; // this matches the format string for #text below
         }
 
-        @Override
-        public long contentLength() throws IOException {
-            return responseBody.contentLength();
+        @Override protected void onConnecting() {
+            progress.setVisibility(View.VISIBLE);
         }
-
-        @Override
-        public BufferedSource source() throws IOException {
-            if (bufferedSource == null) {
-                bufferedSource = Okio.buffer(source(responseBody.source()));
-            }
-            return bufferedSource;
+        @Override protected void onDownloading(long bytesRead, long expectedLength) {
+            progress.setProgress((int)(100 * bytesRead / expectedLength));
         }
-
-        private Source source(Source source) {
-            return new ForwardingSource(source) {
-                long totalBytesRead = 0L;
-
-                @Override
-                public long read(Buffer sink, long byteCount) throws IOException {
-                    long bytesRead = super.read(sink, byteCount);
-                    // read() returns the number of bytes read, or -1 if this source is exhausted.
-                    totalBytesRead += bytesRead != -1 ? bytesRead : 0;
-                    progressListener.update(totalBytesRead, responseBody.contentLength(), bytesRead == -1);
-                    return bytesRead;
-                }
-            };
+        @Override protected void onDownloaded() {
         }
-    }
-
-    interface ProgressListener {
-        void update(long bytesRead, long contentLength, boolean done);
+        @Override protected void onDelivered() {
+            progress.setVisibility(View.GONE);
+        }
     }
 }
